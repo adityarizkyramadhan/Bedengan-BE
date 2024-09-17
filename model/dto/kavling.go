03e44fffaf3739c655b6191b653f8a9d815a64a1
Aditya Rizky Ramadhan <adityarizky1020@gmail.com>
2024-09-17 21:25:23 +0700

    fix(struct): add `binding:"required"` tag to enforce mandatory fields
    - Added `binding:"required"` tag to relevant fields in struct to ensure they are required during binding and validation.

--- model/dto/kavling.go
+++ model/dto/kavling.go
@@ -1,7 +1,7 @@
 package dto
 
 type FindAllKavlingRequest struct {
-	GroundID          string `json:"ground_id" form:"ground_id"`
-	TanggalKedatangan string `json:"tanggal_kedatangan" form:"tanggal_kedatangan"`
-	TanggalKepulangan string `json:"tanggal_kepulangan" form:"tanggal_kepulangan"`
+	GroundID          string `json:"ground_id" form:"ground_id" binding:"required"`
+	TanggalKedatangan string `json:"tanggal_kedatangan" form:"tanggal_kedatangan" binding:"required"`
+	TanggalKepulangan string `json:"tanggal_kepulangan" form:"tanggal_kepulangan" binding:"required"`
 }
