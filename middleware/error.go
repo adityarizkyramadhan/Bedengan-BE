6656b2ba103b0658362324d5f46762bb744a54a9
Aditya Rizky Ramadhan <adityarizky1020@gmail.com>
2024-09-19 07:52:40 +0700

    refactor: update error handling middleware
    - Improved error handling logic for better exception management.
    - Refactored middleware to provide more detailed error messages and status codes.

--- middleware/error.go
+++ middleware/error.go
@@ -1,9 +1,6 @@
 package middleware
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/adityarizkyramadhan/template-go-mvc/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -18,20 +15,6 @@ func ErrorHandler() gin.HandlerFunc {
 		if err != nil {
 			// Tentukan status code dan pesan error yang sesuai
 			errParse := utils.ParseError(err.Error())
-			if errParse.StatusCode >= 500 {
-				// Format [API-Muhammadiyah] IP Address - Method - Path - Status Code - Message
-				message := fmt.Sprintf("[API-Muhammadiyah] %s - %s - %s - %d - %s",
-					ctx.ClientIP(),
-					ctx.Request.Method,
-					ctx.Request.URL.Path,
-					errParse.StatusCode,
-					errParse.Message,
-				)
-				// Kirim pesan error ke Telegram
-				if err := utils.SendTelegramMessage(message); err != nil {
-					log.Println("Failed to send message to Telegram")
-				}
-			}
 			utils.ErrorResponse(ctx, errParse.StatusCode, errParse.Message)
 		}
 	}
